00f97752ab5c48c4939aab275e4b0830b0047142
Zhenya Loyko <e.lojko@inbox.ru>
2023-12-12 08:52:06 +0300

    Project created

---
+++ SwiftUI.1/SwiftUI.1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI.1
+//
+//  Created by Евгений Лойко on 12.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI.1/SwiftUI.1/SwiftUI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_1App.swift
+//  SwiftUI.1
+//
+//  Created by Евгений Лойко on 12.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
